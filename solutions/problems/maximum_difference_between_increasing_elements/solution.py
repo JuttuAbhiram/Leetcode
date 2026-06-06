3f4e753a6ae4e6e57c66035211e39b0797eaa47c
JuttuAbhiram <juttuabhiram@gmail.com>
2026-06-06 05:45:07 +0000

    Sync LeetCode submission - Maximum Difference Between Increasing Elements (python3)

---
+++ solutions/problems/maximum_difference_between_increasing_elements/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumDifference(self, nums: List[int]) -> int:
+        max_diff=-1
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]<nums[j]:
+                    max_diff=max(max_diff,nums[j]-nums[i])      
+        return max_diff
