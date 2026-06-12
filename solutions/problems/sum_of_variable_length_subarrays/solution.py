49fca607a34125792e11ce8350a578d24fb569d8
JuttuAbhiram <juttuabhiram@gmail.com>
2026-06-12 15:37:46 +0000

    Sync LeetCode submission - Sum of Variable Length Subarrays (python3)

---
+++ solutions/problems/sum_of_variable_length_subarrays/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subarraySum(self, nums: List[int]) -> int:
+        n = len(nums)
+        prefix = [0] * (n + 1)
+        for i in range(n):
+            prefix[i + 1] = prefix[i] + nums[i]
+        ans = 0
+        for i in range(n):
+            start = max(0, i - nums[i])
+            ans += prefix[i + 1] - prefix[start]
+        return ans
