cb13241809153ef5e053e5ec26451003216e648c
JuttuAbhiram <juttuabhiram@gmail.com>
2026-06-23 16:18:47 +0000

    Sync LeetCode submission - Remove Outermost Parentheses (python3)

---
+++ solutions/problems/remove_outermost_parentheses/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        count = 0
+        res = []
+        for ch in s:
+            if ch =="(":
+                if count>0:
+                    res.append(ch)
+                count+= 1
+            else:
+                count-= 1
+                if count>0:
+                    res.append(ch)
+        return "".join(res)
