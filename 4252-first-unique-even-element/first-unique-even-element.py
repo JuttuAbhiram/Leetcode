291edb99fc83b1ffe0d83acb3d023827b9288524
JuttuAbhiram <juttuabhiram@gmail.com>
2026-08-06 21:21:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (23.17%) - LeetSync

---
+++ 4252-first-unique-even-element/first-unique-even-element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def firstUniqueEven(self, nums: list[int]) -> int:
+        freq={}
+        for num in nums:
+            if num%2==0:
+                freq[num]=freq.get(num,0)+1
+        for num in nums:
+            if num%2==0 and freq[num]==1:
+                return num
+        return -1
