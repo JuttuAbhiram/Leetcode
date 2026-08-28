62a480a9cc51f9c154c66b8c858e371723703630
JuttuAbhiram <juttuabhiram@gmail.com>
2026-08-28 18:29:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (82.36%) - LeetSync

--- 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.py
+++ 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.py
@@ -1,6 +1,6 @@
 class Solution:
     def smallestEqual(self, nums: List[int]) -> int:
-        for i in range(len(nums)):
-            if i%10==nums[i]:
+        for i,value in enumerate(nums):
+            if i%10==value:
                 return i
         return -1
